089bbe23a81d609a2e737f46a20a37a9f8658921
Andrew Bradshaw <wiggin315@gmail.com>
2013-09-16 03:53:37 -0400

    Modified Wall and added parsing environment

--- src/ourObjects/Constants.java
+++ src/ourObjects/Constants.java
@@ -7,11 +7,12 @@ public interface Constants {
 		CID_MASS		= 0x01,
 		CID_FIXEDMASS 	= 0x02,
 		CID_SPRING 		= 0x04,
-		CID_MUSCLE 		= 0x08;
+		CID_MUSCLE 		= 0x08,
+		CID_WALL		= 0x12;
 	public static final double MASS_RADIUS = 10;
 	public static final double MASS_DEFAULT_MASS = 5;
-	public static final JGColor MASS_COLOR = JGColor.cyan, SPRING_COLOR = JGColor.orange;
-	public static final double DEFAULT_SPRING_CONSTANT = 0;
+	public static final JGColor MASS_COLOR = JGColor.cyan, SPRING_COLOR = JGColor.orange, WALL_COLOR = JGColor.red;
+	public static final double DEFAULT_SPRING_CONSTANT = 1;
 	public static final double WALL_MARGIN = 10;
 	public static final double WALL_THICKNESS = 10;
 
--- src/ourObjects/Wall.java
+++ src/ourObjects/Wall.java
@@ -1,77 +1,21 @@
 package ourObjects;
 
+import jboxGlue.PhysicalObjectRect;
 import jboxGlue.WorldManager;
+import jgame.JGColor;
 
 import org.jbox2d.collision.PolygonDef;
 
-public class Wall extends Mass {
-
-	private double myWidth;
-	private double myHeight;
-	private boolean myHorizontal;
-	private double[] myPolyx;
-	private double[] myPolyy;
-	
-	public Wall(String animId, double x, double y, double width, double height, boolean isHorizontal) {
-		super(animId, x, y);
-		myHorizontal = isHorizontal;
-		init(width, height);
-	}
-	
-	public void init(double width, double height) {
-		myWidth = width;
-		myHeight = height;
-		
-		myPolyx = null;
-		myPolyy = null;
-		
-		// make it a rect
-		PolygonDef shape = new PolygonDef();
-		shape.density = (float) 0;
-		shape.setAsBox( (float)width, (float)height );
-		createBody( shape );
-		setBBox( -(int)width/2, -(int)height/2, (int)width, (int)height );
-		
-	}
-	
-	public void paintShape() {
-		if( myPolyx == null || myPolyy == null )
-		{
-			// allocate memory for the polygon
-			myPolyx = new double[4];
-			myPolyy = new double[4];
-		}
-		
-		myEngine.setColor( myColor );
-		double cos = Math.cos( myRotation );
-		double sin = Math.sin( myRotation );
-		double halfWidth = myWidth/2;
-		double halfHeight = myHeight/2;
-		myPolyx[0] = (int)( x - halfWidth * cos - halfHeight * sin );
-		myPolyy[0] = (int)( y + halfWidth * sin - halfHeight * cos );
-		myPolyx[1] = (int)( x + halfWidth * cos - halfHeight * sin );
-		myPolyy[1] = (int)( y - halfWidth * sin - halfHeight * cos );
-		myPolyx[2] = (int)( x + halfWidth * cos + halfHeight * sin );
-		myPolyy[2] = (int)( y - halfWidth * sin + halfHeight * cos );
-		myPolyx[3] = (int)( x - halfWidth * cos + halfHeight * sin );
-		myPolyy[3] = (int)( y + halfWidth * sin + halfHeight * cos );
-		myEngine.drawPolygon( myPolyx, myPolyy, null, 4, true, true );
+public class Wall extends PhysicalObjectRect {
+	private double myMagnitude;
+	private double myExponent;
+	public Wall(String id, double width, double height, double magnitude, double exponent) {
+		super(id,Constants.CID_WALL, Constants.WALL_COLOR, width, height);
+		myMagnitude = magnitude;
+		myExponent = exponent;
 	}
 	
 	public void repelMasses(){
-		ArrayList<Mass> masses = WorldManager.getWorld().getMasses();
-		
-		for(Mass M : masses) {
-			double distance = 0;
-			// If wall is horizontal, only care about X distance from wall
-			// If wall is vertical, only care about Y distance from wall
-			if(myHorizontal){
-				distance = Math.abs(M.getX()-getX());
-			}
-			else {
-				distance = Math.abs(M.getY()-getY());
-			}
-		}
 		
 	}
 
